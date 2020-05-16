bfd8a1af76b8ada8d119356a34899e591f8a47da
Amargy <asdawydov@gmail.com>
2020-05-17 01:57:25 +0300

    brutalsteps

--- src/main.c
+++ src/main.c
@@ -384,8 +384,27 @@ void	render_rays(t_wolf3d *blazko)
 
 void	play_step(t_wolf3d *blazko)
 {
+	int r;
+
+	r = 0;
+	srand(time(NULL));
+	while (r == 0)
+		r = rand() % 6;
 	if (!Mix_Playing(2))
-		Mix_PlayChannel(2, blazko->sound.step, 0);	
+	{
+	if (r == 1)
+		Mix_PlayChannel(2, blazko->sound.s1, 0);
+	if (r == 2)
+		Mix_PlayChannel(2, blazko->sound.s2, 0);
+	if (r == 3)
+		Mix_PlayChannel(2, blazko->sound.s3, 0);
+	if (r == 4)
+		Mix_PlayChannel(2, blazko->sound.s4, 0);
+	if (r == 5)
+		Mix_PlayChannel(2, blazko->sound.s5, 0);
+	if (r == 6)
+		Mix_PlayChannel(2, blazko->sound.s6, 0);
+	 }
 }
 
 void	stop_step(t_wolf3d *blazko)
@@ -394,7 +413,8 @@ void	stop_step(t_wolf3d *blazko)
 	&& blazko->player.walk_direction == 0
 	&& blazko->player.turn_direction == 0
 	&& blazko->player.turn_direction == 0)
-	Mix_HaltChannel(2);
+		Mix_FadeOutChannel(2, 200);
+		//Mix_HaltChannel(2);
 }
 
 void	input(int *game_on, t_wolf3d *blazko)
@@ -448,22 +468,22 @@ void	input(int *game_on, t_wolf3d *blazko)
 		if (event.key.keysym.sym == SDLK_UP)
 		{
 			blazko->player.walk_direction = 0;
-			stop_step(blazko);
+			//stop_step(blazko);
 		}
 		if (event.key.keysym.sym == SDLK_DOWN)
 		{
 			blazko->player.walk_direction = 0;
-			stop_step(blazko);
+			//stop_step(blazko);
 		}
 		if (event.key.keysym.sym == SDLK_RIGHT)
 		{
 			blazko->player.turn_direction = 0;
-			stop_step(blazko);
+			//stop_step(blazko);
 		}
 		if (event.key.keysym.sym == SDLK_LEFT)
 		{
 			blazko->player.turn_direction = 0;
-			stop_step(blazko);
+			//stop_step(blazko);
 		}
 	}
 	return ;
@@ -592,6 +612,7 @@ void	render(t_wolf3d *blazko)
 	render_player(blazko);
 	
 	SDL_RenderPresent(blazko->render);
+	stop_step(blazko);
 }
 
 void	music_on(t_wolf3d *blazko)
@@ -600,10 +621,21 @@ void	music_on(t_wolf3d *blazko)
 	if(Mix_OpenAudio(48000, MIX_DEFAULT_FORMAT, 2, 4096))
 		ft_error("very bad music");
 	blazko->sound.badmusic = Mix_LoadMUS("sound/music.ogg");
-	blazko->sound.step = Mix_LoadWAV("sound/step.ogg");
-	if (blazko->sound.badmusic == NULL || blazko->sound.step == NULL)
+	blazko->sound.s1 = Mix_LoadWAV("sound/steps/1.ogg");
+	blazko->sound.s2 = Mix_LoadWAV("sound/steps/2.ogg");
+	blazko->sound.s3 = Mix_LoadWAV("sound/steps/3.ogg");
+	blazko->sound.s4 = Mix_LoadWAV("sound/steps/4.ogg");
+	blazko->sound.s5 = Mix_LoadWAV("sound/steps/5.ogg");
+	blazko->sound.s6 = Mix_LoadWAV("sound/steps/6.ogg");
+	if (blazko->sound.badmusic == NULL
+	|| blazko->sound.s1 == NULL
+	|| blazko->sound.s2 == NULL
+	|| blazko->sound.s3 == NULL
+	|| blazko->sound.s4 == NULL
+	|| blazko->sound.s5 == NULL
+	|| blazko->sound.s6 == NULL)
 		ft_error("very bad music");
-	Mix_AllocateChannels(10);
+	Mix_AllocateChannels(23);
 	Mix_VolumeMusic(15);
 }
 
