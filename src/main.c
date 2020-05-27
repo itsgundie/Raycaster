81e520a418668b42a9cea62d9c64aefc265f3245
Amargy <asdawydov@gmail.com>
2020-05-28 00:37:28 +0300

    main norm

--- src/main.c
+++ src/main.c
@@ -2,43 +2,40 @@
 
 void	make_a_move(t_wolf3d *blazko, float delta_time)
 {
-	float move_step;
+	float	move_step;
 	t_v2	new_pos;
 
-	printf( "player angle before move is %f\n",blazko->player.rotation_angle);
-	printf( "player before move x - %f and y - %f\n", blazko->player.pos.x, blazko->player.pos.y);
 	blazko->player.rotation_angle += (((blazko->player.turn_direction)
 			* (blazko->player.rotate_speed)));
-	blazko->player.rotation_angle = normalize_angle(blazko->player.rotation_angle);
+	blazko->player.
+	rotation_angle = normalize_angle(blazko->player.rotation_angle);
 	move_step = blazko->player.walk_direction * blazko->player.move_speed;
-	new_pos.x = (blazko->player.pos.x + cos(blazko->player.rotation_angle) * move_step);
-	new_pos.y = (blazko->player.pos.y + sin(blazko->player.rotation_angle) * move_step);
-	printf("this is a move_step - %f\n", move_step);
-	printf("this is a new_pos.x - %f\n", new_pos.x);
-	printf("this is a new_pos.y - %f\n", new_pos.y);
-	if (!(find_an_obstacle(floor(new_pos.x) ,floor(new_pos.y), &(blazko->map))))
+	new_pos.x = (blazko->player.pos.x
+	+ cos(blazko->player.rotation_angle) * move_step);
+	new_pos.y = (blazko->player.pos.y
+	+ sin(blazko->player.rotation_angle) * move_step);
+	if (!(find_an_obstacle(floor(new_pos.x), floor(new_pos.y), &(blazko->map))))
 	{
 		blazko->player.pos.x = new_pos.x;
 		blazko->player.pos.y = new_pos.y;
 	}
-	printf( "player angle after move is %f\n",blazko->player.rotation_angle);
-	printf( "player after move x - %f and y - %f\n", blazko->player.pos.x, blazko->player.pos.y);
 }
 
 void	update(t_wolf3d *blazko, long long *ticks_last_frame)
 {
-	int time_to_wait;
-	time_to_wait = blazko->frame_time - (SDL_GetTicks() - *ticks_last_frame);
+	int		time_to_wait;
+	float	delta_time;
+	Uint32	ticks;
 
+	time_to_wait = blazko->frame_time - (SDL_GetTicks() - *ticks_last_frame);
 	if (time_to_wait > 0 && time_to_wait <= blazko->frame_time)
 		SDL_Delay(time_to_wait);
-
-	while(!(SDL_TICKS_PASSED(SDL_GetTicks(), (*ticks_last_frame + blazko->frame_time))));
-
-	float delta_time = (SDL_GetTicks() - *ticks_last_frame) * 1000.0f;
+	ticks = SDL_GetTicks();
+	while (!(SDL_TICKS_PASSED(ticks, (*ticks_last_frame + blazko->frame_time))))
+		ticks = SDL_GetTicks();
+	delta_time = (SDL_GetTicks() - *ticks_last_frame) * 1000.0f;
 	*ticks_last_frame = SDL_GetTicks();
-	//make_a_move(blazko, delta_time);
-	raycast(blazko); //?
+	raycast(blazko);
 }
 
 void	clear_color_buf(uint32_t *color_buf, uint32_t color)
@@ -57,9 +54,9 @@ void	clear_color_buf(uint32_t *color_buf, uint32_t color)
 
 int		main(int argc, char **argv)
 {
-	t_wolf3d *blazko;
-	int fd;
-	long long ticks_last_frame;
+	t_wolf3d	*blazko;
+	int			fd;
+	long long	ticks_last_frame;
 
 	if (argc != 2 || (fd = open(argv[1], O_RDONLY)) < 0)
 		ft_error("usage: ./wolf3d map");
@@ -70,9 +67,7 @@ int		main(int argc, char **argv)
 		create_map(blazko);
 	setup(blazko);
 	music_on(blazko);
-	int i = 0;
-	int i2 = 0;
-	while(1)
+	while (1)
 	{
 		if (!(put_in(blazko)))
 			break ;
@@ -81,5 +76,5 @@ int		main(int argc, char **argv)
 		render(blazko);
 	}
 	destroy(blazko);
-	return(0);
+	return (0);
 }
